d6b0d96426f0fe69ed1ec4f4c47e0228ddc5673c
taniya76 <www.taniyamittalhgn@gmail.com>
2021-08-08 00:45:06 +0530

    changing the text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Testing by me
+          Testing by Taniya
         </a>
       </header>
     </div>
